f9e30560cc18267b1ace60694a3835c23c8b37f1
Maximiliano Firtman <firtman@gmail.com>
2022-08-10 12:32:27 -0300

    multiple output, final code

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"sync"
 
 	"andreani.com/go/weathergo/api"
@@ -11,20 +14,60 @@ import (
 func main() { // main goroutine
 	fmt.Println("Weather Go 1.0 - Welcome")
 
-	cities := os.Args[1:]
+	citiesPtr := flag.String("cities", "", "List of comma-separated cities")
+	inputPtr := flag.String("input", "std", "How to get the cities to process, valid values: std, scan")
+	outputPtr := flag.String("output", "std", "How to output the weather data, valid values: std, json")
+	flag.Parse()
 
-	var wg sync.WaitGroup
+	var outputFunction func(city string)
 
-	for _, city := range cities { // for each
-		wg.Add(1)
-		fmt.Printf("Looking for weather in %v ⏳\n", city) // sync
-		go func(currentCity string) {                     // goroutine en un bloque anónimo
-			processCity(currentCity)
-			wg.Done() // wait synchronously to the whole group
-		}(city)
+	switch *outputPtr {
+	case "std":
+		outputFunction = processCity
+	case "json":
+		outputFunction = processCityToJson
 	}
-	wg.Wait()
+
+	switch *inputPtr {
+	case "std":
+		cities := strings.Split(*citiesPtr, ",")
+		var wg sync.WaitGroup
+
+		for _, city := range cities { // for each
+			wg.Add(1)
+			fmt.Printf("Looking for weather in %v ⏳\n", city) // sync
+			go func(currentCity string) {                     // goroutine en un bloque anónimo
+				outputFunction(currentCity)
+				wg.Done() // wait synchronously to the whole group
+			}(city)
+		}
+		wg.Wait()
+	case "scan":
+		fmt.Print("Enter the city name: ")
+		var userCity string
+		fmt.Scan(&userCity)
+		outputFunction(userCity)
+	}
+
 	// time.Sleep(2 * time.Second) // espero en el main goroutine 3 segundos
+}
+
+func processCityToJson(city string) {
+	wc, err := api.GetWeather(city) // sincrónico
+	if err != nil {
+		fmt.Println("We couldn't fetch the weather")
+	} else {
+		bytes, err := json.Marshal(wc)
+		if err != nil {
+			fmt.Println("Invalid JSON generation")
+		} else {
+			if _, err := os.Stat("output"); os.IsNotExist(err) {
+				os.Mkdir("output", 0)
+			}
+			filename := fmt.Sprintf("output/%v.json", city)
+			os.WriteFile(filename, bytes, 0)
+		}
+	}
 }
 
 func processCity(city string) {
