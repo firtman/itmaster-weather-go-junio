09851e0f6d8ac8978ecd4f7b90c5eb8840452fe8
Maximiliano Firtman <firtman@gmail.com>
2022-08-10 11:02:27 -0300

    multiple city async

--- main.go
+++ main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"andreani.com/go/weathergo/api"
 )
 
-func main() {
+func main() { // main couroutine
 	fmt.Println("Weather Go 1.0 - Welcome")
 	cities := []string{"Madrid", "Buenos Aires", "Rosario", "Moscow"}
 
 	for _, city := range cities { // for each
-		fmt.Printf("Looking for weather in %v ⏳\n", city)
-		wc, err := api.GetWeather(city) // sincrónico
-		if err != nil {
-			fmt.Println(err.Error())
-		} else {
-			fmt.Printf("The temperature for %v is %.1f⁰C\n",
-				wc.Name, wc.Temperature.ToCelsius())
-		}
+		fmt.Printf("Looking for weather in %v ⏳\n", city) // sync
+		go processCity(city)                              // async
 	}
 
+	time.Sleep(2 * time.Second) // espero en el main goroutine 3 segundos
+}
+
+func processCity(city string) {
+	wc, err := api.GetWeather(city) // sincrónico
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("The temperature for %v is %.1f⁰C\n",
+			wc.Name, wc.Temperature.ToCelsius())
+	}
 }
