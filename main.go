6c3f2c2378dfb9b8e99947eb5170ae870c5a44d9
Maximiliano Firtman <firtman@gmail.com>
2022-08-10 10:37:35 -0300

    multiple cities sync

--- main.go
+++ main.go
@@ -8,11 +8,17 @@ import (
 
 func main() {
 	fmt.Println("Weather Go 1.0 - Welcome")
-	wc, err := api.GetWeather("Madrid")
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Printf("The temperature for %v is %.1f⁰C\n",
-			wc.Name, wc.Temperature.ToCelsius())
+	cities := []string{"Madrid", "Buenos Aires", "Rosario", "Moscow"}
+
+	for _, city := range cities { // for each
+		fmt.Printf("Looking for weather in %v ⏳\n", city)
+		wc, err := api.GetWeather(city) // sincrónico
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			fmt.Printf("The temperature for %v is %.1f⁰C\n",
+				wc.Name, wc.Temperature.ToCelsius())
+		}
 	}
+
 }
